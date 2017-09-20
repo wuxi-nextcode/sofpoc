98cedc3fc24c3ac9afee8edd2f20c16fd0c30736
Hlynur Tryggvason <hlynur@wuxinextcode.com>
2017-09-20 17:27:56 +0000

    adding some css variables, adding fhir client cerner additions library

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './styles/main.scss';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import './vendor/fhir-client-cerner-additions-1.0.0.js'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
